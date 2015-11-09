018af31e1327d9c2867a5221032696c05406005d
elt <elt@eltdeimac.local>
2015-11-09 10:15:56 +0800

    Initial Commit

---
+++ EltDoctor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EltDoctor
+//
+//  Created by elt on 15/11/9.
+//  Copyright © 2015年 elt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EltDoctor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EltDoctor
+//
+//  Created by elt on 15/11/9.
+//  Copyright © 2015年 elt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
